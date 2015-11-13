d271437cd5eabe9a1a5056f89e6af1013a6e4002
pochaevets <pochaevets.a@gmail.com>
2015-11-13 16:44:07 +0300

    BITS-705

--- device/app/Script/requests/choiceproduct.js
+++ device/app/Script/requests/choiceproduct.js
@@ -22,28 +22,19 @@ function GetProducts(objCust, searchString, vRef) {
 	//Dialog.Debug(searchString);
 	var q = new Query();
 	var qt = "SELECT P.Id AS Id, P.Product AS Product, S.Description AS Description FROM Catalog_Customer_Products P LEFT JOIN Catalog_SKU S ON P.Product = S.Id WHERE P.Ref = @cust";
-	var qq = new Query();
-	var qqt = "SELECT P.Id AS Id, P.Product AS Product, S.Description AS Description FROM Catalog_Customer_Products P LEFT JOIN Catalog_SKU S ON P.Product = S.Id WHERE";
 	if (searchString != "" && searchString != null) {
 		var plus = " AND  Contains(S.Description, @st)";
-		var qplus = " Contains(S.Description, @st) AND";
 		qt = qt + plus;
-		qqt = qqt + qplus;
 		q.AddParameter("st", searchString);
-		qq.AddParameter("st", searchString);
 	}
 	//Dialog.Debug(qt);
 	q.Text = qt;
-	qq.Text = qqt + " NOT P.Product IN (SELECT SKU FROM Document_Visit_Result WHERE Document_Visit_Result.Ref = @r) LIMIT 150";
 	q.AddParameter("cust", objCust);
 	if (q.ExecuteCount() > 0){
 		q.Text = qt + " AND NOT P.Product IN (SELECT SKU FROM Document_Visit_Result WHERE Document_Visit_Result.Ref = @r)";
-		q.AddParameter("r", vRef);
-		return q.Execute();	
-	} else {
-		qq.AddParameter("r", vRef);		
-		return qq.Execute();
-	}
+		q.AddParameter("r", vRef);			
+	} 
+	return q.Execute();
 }
 
 function GetAllProducts(searchString, vRef) {
