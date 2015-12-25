5e63df8adda9035d284a878a8e76c3d79b6ee524
pochaevets <pochaevets.a@gmail.com>
2015-12-25 16:53:09 +0300

    BITS-759 Дополнили механизм работы плановым временем в визите

--- device/app/Script/requests/customer.js
+++ device/app/Script/requests/customer.js
@@ -163,9 +163,16 @@ function CallBackEndPlan(state,args){
 	if (args.Result > getEndOfDay(obj.PlanStartDataTime)) {
 		obj.PlanEndDataTime = getEndOfDay(obj.PlanStartDataTime);
 		$.endDate.Text = DoFullDate(getEndOfDay(obj.PlanStartDataTime));
+		Dialog.Message("Плановая дата окончания не может быть позже 1 дня от даты начала.");
 	} else {
-		obj.PlanEndDataTime = args.Result;
-		$.endDate.Text = DoFullDate(args.Result);
+		if (args.Result >= Date(obj.PlanStartDataTime)) {
+			obj.PlanEndDataTime = args.Result;
+			$.endDate.Text = DoFullDate(args.Result);
+		} else {
+			obj.PlanEndDataTime = obj.PlanStartDataTime;
+			$.endDate.Text = DoFullDate(obj.PlanStartDataTime);
+			Dialog.Message("Плановая дата окончания не может быть раньше плановой даты начала.");
+		}
 	}
 
 	obj.Save(false);
